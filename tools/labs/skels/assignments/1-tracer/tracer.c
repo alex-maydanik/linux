6330a53398c1fb9b37dda959566ce75e0393c8b3
Alex Maydanik <alexander.maydanik@gmail.com>
2021-08-07 12:35:21 +0300

    TRACER: Added procfs entry for reading trace data

--- tools/labs/skels/assignments/1-tracer/tracer.c
+++ tools/labs/skels/assignments/1-tracer/tracer.c
@@ -15,12 +15,19 @@
 #include <linux/hashtable.h>
 #include <linux/slab.h>
 #include <linux/sched.h>
+#include <linux/rwsem.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
 
 #include "tracer.h"
 
 MODULE_DESCRIPTION("Kprobe based tracer");
 MODULE_AUTHOR("Alexander Maydanik <alexander.maydanik@gmail.com>");
 MODULE_LICENSE("GPL v2");
+
+#define TRACER_PROCFS_NAME		"tracer"
+
+struct proc_dir_entry *proc_tracer;
 
 /*
  * Holds tracing data for a single process.
@@ -42,20 +49,19 @@ struct trace_data {
 	u64 lock_calls;
 	u64 unlock_calls;
 
-	spinlock_t lock;
 	struct hlist_node hnode;
 };
 
 #define TRACED_PROCS_HASH_BITS	5
 static DEFINE_HASHTABLE(traced_procs_hash, TRACED_PROCS_HASH_BITS);
-static DEFINE_MUTEX(traced_procs_lock);
+static DECLARE_RWSEM(traced_procs_lock);
 
 static int tracer_add_process(pid_t pid)
 {
 	struct trace_data *td;
 	int ret = 0;
 
-	mutex_lock(&traced_procs_lock);
+	down_write(&traced_procs_lock);
 
 	/* Check if process exists in the system */
 	if (!find_vpid(pid)) {
@@ -82,7 +88,7 @@ static int tracer_add_process(pid_t pid)
 	hash_add(traced_procs_hash, &td->hnode, pid);
 
 error:
-	mutex_unlock(&traced_procs_lock);
+	up_write(&traced_procs_lock);
 
 	return ret;
 }
@@ -92,7 +98,7 @@ static int tracer_remove_process(pid_t pid)
 {
 	struct trace_data *td;
 
-	mutex_lock(&traced_procs_lock);
+	down_write(&traced_procs_lock);
 
 	/* Find process */
 	hash_for_each_possible(traced_procs_hash, td, hnode, pid) {
@@ -100,12 +106,12 @@ static int tracer_remove_process(pid_t pid)
 			goto found;
 	}
 
-	mutex_unlock(&traced_procs_lock);
+	up_write(&traced_procs_lock);
 	return -ESRCH;
 
 found:
-	hash_del_rcu(&td->hnode);
-	mutex_unlock(&traced_procs_lock);
+	hash_del(&td->hnode);
+	up_write(&traced_procs_lock);
 	kfree(td);
 
 	return 0;
@@ -130,6 +136,39 @@ tracer_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 	return ret;
 }
 
+static int tracer_proc_show(struct seq_file *m, void *v)
+{
+	struct trace_data *td;
+	int i;
+
+	seq_printf(m, "%-6s%-8s%-6s%-12s%-11s%-8s%-7s%-7s%-6s%-6s\n",
+		"PID", "kmalloc", "kfree", "kmalloc_mem", "kfree_mem", "sched",
+		"up", "down", "lock", "unlock");
+
+	down_read(&traced_procs_lock);
+	hash_for_each(traced_procs_hash, i, td, hnode) {
+		seq_printf(m, "%-6d%-8llu%-6llu%-12llu%-11llu%-8llu%-7llu%-7llu%-6llu%-6llu\n",
+			td->pid, td->kmalloc_calls, td->kfree_calls,
+			td->kmalloc_mem, td->kfree_mem, td->sched_calls,
+			td->up_calls, td->down_calls,
+			td->lock_calls, td->unlock_calls);
+	}
+	up_read(&traced_procs_lock);
+
+	return 0;
+}
+
+static int tracer_procfs_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, tracer_proc_show, NULL);
+}
+
+static const struct proc_ops r_pops = {
+	.proc_open		= tracer_procfs_open,
+	.proc_read		= seq_read,
+	.proc_release	= single_release,
+};
+
 static const struct file_operations tracer_fops = {
 	.owner = THIS_MODULE,
 	.unlocked_ioctl = tracer_ioctl,
@@ -143,11 +182,17 @@ static struct miscdevice tracer_device = {
 
 static int tracer_init(void)
 {
-	int err;
+	int err = 0;
 
 	err = misc_register(&tracer_device);
-	if (err != 0) {
+	if (err != 0)
 		return err;
+		
+	proc_tracer= proc_create(TRACER_PROCFS_NAME, 0000, proc_tracer, &r_pops);
+	if (!proc_tracer) {
+		proc_remove(proc_tracer);
+		misc_deregister(&tracer_device);
+		return -ENOMEM;
 	}
 
 	return 0;
@@ -160,13 +205,14 @@ static void tracer_exit(void)
 	int i;
 
 	/* Free hashtable */
-	mutex_lock(&traced_procs_lock);
+	down_write(&traced_procs_lock);
 	hash_for_each_safe(traced_procs_hash, i, tmp, td, hnode) {
 		hash_del(&td->hnode);
 		kfree(td);
 	}	
-	mutex_unlock(&traced_procs_lock);
+	up_write(&traced_procs_lock);
 
+	proc_remove(proc_tracer);
 	misc_deregister(&tracer_device);
 }
 
