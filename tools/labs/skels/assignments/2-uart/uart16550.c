b876b455c4f622c0794b2843e75ba8e114c79afd
Alex Maydanik <alexander.maydanik@gmail.com>
2021-09-04 13:14:10 +0300

    UART: Added support for module 'major' and 'option' parameters

--- tools/labs/skels/assignments/2-uart/uart16550.c
+++ tools/labs/skels/assignments/2-uart/uart16550.c
@@ -10,6 +10,7 @@
 #include <linux/fs.h>
 #include <linux/cdev.h>
 #include <linux/uaccess.h>
+#include <linux/moduleparam.h>
 
 #include "uart16550.h"
 
@@ -18,7 +19,16 @@ MODULE_AUTHOR("Alexander Maydanik <alexander.maydanik@gmail.com>");
 MODULE_LICENSE("GPL v2");
 
 #define MODULE_NAME			"uart16550"
-#define UART16550_DEFAULT_MAJOR		42
+
+static int major = 	42;
+static int option =	OPTION_BOTH;
+static int start_minor;
+static int num_devices;
+
+module_param(major, int, 0);
+MODULE_PARM_DESC(major, "UART16550 character device major");
+module_param(option, int, 0);
+MODULE_PARM_DESC(option, "UART16550 operation option: OPTION_BOTH (default) / OPTION_COM1 / OPTION_COM2");
 
 struct uart16550 {
 	struct cdev cdev;
@@ -58,20 +68,48 @@ static const struct file_operations uart16550_fops = {
 	.unlocked_ioctl = uart16550_ioctl,
 };
 
+static int parse_option_arg(void)
+{
+	switch (option) {
+	case OPTION_COM1:
+		start_minor = 0;
+		num_devices = 1;
+		break;
+	case OPTION_COM2:
+		start_minor = 1;
+		num_devices = 1;
+		break;
+	case OPTION_BOTH:
+		start_minor = 0;
+		num_devices = 2;
+		break;
+	default:
+		pr_err("%s: option parameter must be one of: %d,%d,%d\n",
+			MODULE_NAME, OPTION_COM1, OPTION_COM2, OPTION_BOTH);
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
 static int uart16550_init(void)
 {
 	int err, i;
 
-	err = register_chrdev_region(MKDEV(UART16550_DEFAULT_MAJOR, 0),
-				     MAX_NUMBER_DEVICES, MODULE_NAME);
+	err = parse_option_arg();
+	if (err != 0)
+		goto out;
+	
+	err = register_chrdev_region(MKDEV(major, start_minor),
+				     num_devices, MODULE_NAME);
 	if (err != 0) {
 		pr_err("%s: register_chrdev_region() failed: %d\n", MODULE_NAME, err);
 		goto out;
 	}
 
-	for (i = 0; i < MAX_NUMBER_DEVICES; i++) {
+	for (i = start_minor; i < start_minor + num_devices; i++) {
 		cdev_init(&devs[i].cdev, &uart16550_fops);
-		err = cdev_add(&devs[i].cdev, MKDEV(UART16550_DEFAULT_MAJOR, i), 1);
+		err = cdev_add(&devs[i].cdev, MKDEV(major, i), 1);
 		if (err != 0) {
 			pr_err("%s: cdev_add() failed: %d\n", MODULE_NAME, err);
 			goto out_unregister;
@@ -82,10 +120,10 @@ static int uart16550_init(void)
 	return 0;
 
 out_unregister:
-	for (i = 0; i < MAX_NUMBER_DEVICES; i++)
+	for (i = start_minor; i < start_minor + num_devices; i++)
 		cdev_del(&devs[i].cdev);
-	unregister_chrdev_region(MKDEV(UART16550_DEFAULT_MAJOR, 0),
-				 MAX_NUMBER_DEVICES);
+	unregister_chrdev_region(MKDEV(major, start_minor),
+				 num_devices);
 out:
 	return err;
 }
@@ -94,11 +132,11 @@ static void uart16550_exit(void)
 {
 	int i;
 
-	for (i = 0; i < MAX_NUMBER_DEVICES; i++)
+	for (i = start_minor; i < start_minor + num_devices; i++)
 		cdev_del(&devs[i].cdev);
 
-	unregister_chrdev_region(MKDEV(UART16550_DEFAULT_MAJOR, 0),
-				 MAX_NUMBER_DEVICES);
+	unregister_chrdev_region(MKDEV(major, start_minor),
+				 num_devices);
 
 	pr_notice("%s: unloaded\n", MODULE_NAME);
 }
