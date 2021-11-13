ef0fcdd57b476bd98208958b86e51bfd536ac4b8
Alex Maydanik <alexander.maydanik@gmail.com>
2021-11-13 22:16:47 +0200

    SSR: Implemented crc32 functionality. Support for write() only without recovery.

--- tools/labs/skels/assignments/3-raid/ssr.c
+++ tools/labs/skels/assignments/3-raid/ssr.c
@@ -16,8 +16,10 @@
 #include <linux/blk-mq.h>
 #include <linux/bio.h>
 #include <linux/vmalloc.h>
+#include <linux/mutex.h>
 
 #include <linux/workqueue.h>
+#include <linux/crc32.h>
 
 #include "ssr.h"
 
@@ -26,16 +28,20 @@ MODULE_AUTHOR("Alexander Maydanik <alexander.maydanik@gmail.com>");
 MODULE_LICENSE("GPL v2");
 
 #define SSR_MODULE_NAME "ssr"
+#define SSR_NUM_PHYSICAL_DISKS 2
 
 static struct ssr_block_dev {
 	struct gendisk *logical_disk;
 	struct request_queue *queue;
-	struct block_device *physical_disk1, *physical_disk2;
+	struct block_device *physical_disks[SSR_NUM_PHYSICAL_DISKS];
 
 	/* Work queue for submitting bio requests to the physical disks */
 	struct workqueue_struct *physical_wq;
 	/* bio_set for cloned BIO requests */
 	struct bio_set ssr_bioset;
+
+	/* Used to make sure only 1 active submit_bio handled a time */
+	struct mutex lock;
 } ssr_dev;
 
 /* Struct representing a single bio request work item */
@@ -52,7 +58,7 @@ static void ssr_bio_rq_work_handler(struct work_struct *work)
 	struct ssr_bio_req *rq = container_of(work, struct ssr_bio_req, work);
 	struct ssr_block_dev *dev = rq->dev;
 
-	struct bio *bio_copy = bio_clone_fast(rq->bio, GFP_KERNEL, &dev->ssr_bioset);
+	struct bio *bio_copy = bio_clone_fast(rq->bio, GFP_NOIO, &dev->ssr_bioset);
 	if (!bio_copy) {
 		rq->err = -ENOMEM;
 		return;
@@ -63,12 +69,98 @@ static void ssr_bio_rq_work_handler(struct work_struct *work)
 	bio_put(bio_copy);
 }
 
+static inline sector_t sector_to_crc32_sector(sector_t sector)
+{
+	sector_t crc32_sector = sector * sizeof(u32) / KERNEL_SECTOR_SIZE;
+	return LOGICAL_DISK_SECTORS + crc32_sector;
+}
+
+static inline unsigned int sector_to_crc32_sector_offset(sector_t sector)
+{
+	return (sector * sizeof(u32)) % KERNEL_SECTOR_SIZE;
+}
+
+/* 
+ * Allocates and returns `bio` request for reading crc32 sectors matching `data_bio`.
+ * Caller is responsible to free both physical pages and the bio struct.
+ */
+static struct bio *ssr_create_crc32_bio_read(struct bio *data_bio)
+{
+	int i;
+	sector_t sector = data_bio->bi_iter.bi_sector;
+	u32 num_sectors = bio_sectors(data_bio);
+	u32 num_crc32_pages = roundup(num_sectors * sizeof(u32), PAGE_SIZE) / PAGE_SIZE;
+	
+	struct bio *bio_crc32 = bio_alloc(GFP_NOIO, num_crc32_pages);
+	if (!bio_crc32)
+		return NULL;
+	
+	/* 
+	 * Specify start sector & direction.
+	 */
+	bio_crc32->bi_disk = data_bio->bi_disk;
+	bio_crc32->bi_iter.bi_sector = sector_to_crc32_sector(sector);
+	bio_crc32->bi_opf = REQ_OP_READ;
+
+	/* Allocate and add pages */
+	for (i = 0; i < num_crc32_pages; i++) {
+		struct page *page = alloc_page(GFP_NOIO);
+		if (!page)
+			goto failure;
+		if (bio_add_page(bio_crc32, page, PAGE_SIZE, 0) != PAGE_SIZE)
+			goto failure;
+	}
+
+	return bio_crc32;
+
+failure:
+	bio_free_pages(bio_crc32);
+	bio_put(bio_crc32);
+	return NULL;
+}
+
+/* Computes & updates crc32 over `bio` pages and updates `bio_crc32 */
+static void ssr_compute_crc32(struct bio *bio, struct bio *bio_crc32)
+{
+	int i;
+	struct bio_vec bvec, bvec_crc32;
+	struct bvec_iter iter, iter_crc32;
+
+	bio_for_each_segment(bvec, bio, iter) {
+		sector_t sector = iter.bi_sector;
+		char *data = kmap_atomic(bvec.bv_page);
+
+		/* Go over all sectors in the page */
+		for (i = 0; i < bvec.bv_len / KERNEL_SECTOR_SIZE; i++, sector++) {
+			u32 crc32 = crc32(0, data + i * KERNEL_SECTOR_SIZE, KERNEL_SECTOR_SIZE);
+
+			/* Go to matching crc32 page & update */
+			bio_for_each_segment(bvec_crc32, bio_crc32, iter_crc32) {
+				sector_t sector_crc32 = iter_crc32.bi_sector;
+
+				if (sector_to_crc32_sector(sector) >= sector_crc32 &&
+				    sector_to_crc32_sector(sector) < sector_crc32 + PAGE_SIZE / KERNEL_SECTOR_SIZE) {
+					char *crc32_page = kmap_atomic(bvec_crc32.bv_page);
+
+					u32 crc32_offset = sector_to_crc32_sector_offset(sector);
+					memcpy(crc32_page + crc32_offset, &crc32, sizeof(u32));
+
+					kunmap_atomic(crc32_page);
+					break;
+				}
+			}
+		}
+
+		kunmap_atomic(data);
+	}
+}
+
 /* Creates bio request and submits to the specified physical device */
 static int _ssr_submit_bio(struct bio *bio, struct block_device *phy_dev)
 {
 	int err;
 	struct ssr_block_dev *dev = bio->bi_disk->private_data;
-	struct ssr_bio_req *rq = kzalloc(sizeof(*rq), GFP_ATOMIC);
+	struct ssr_bio_req *rq = kzalloc(sizeof(*rq), GFP_NOIO);
 	if (!rq) {
 		err = -ENOMEM;
 		goto end;
@@ -92,24 +184,52 @@ end:
 
 static blk_qc_t ssr_submit_bio(struct bio *bio)
 {
+	int i;
 	struct ssr_block_dev *dev = bio->bi_disk->private_data;
+	struct bio *bio_crc32 = ssr_create_crc32_bio_read(bio);
+
+	mutex_lock(&ssr_dev.lock);
+	if (!bio_crc32)
+		goto bio_error;
 
 	if (bio_data_dir(bio) == READ) {
-		if (_ssr_submit_bio(bio, dev->physical_disk1) != 0)
+		/* Read */
+		if (_ssr_submit_bio(bio, dev->physical_disks[0]) != 0)
 			goto bio_error;
 	} else {
-		/* Write */
-		if (_ssr_submit_bio(bio, dev->physical_disk1) != 0)
-			goto bio_error;
-		if (_ssr_submit_bio(bio, dev->physical_disk2) != 0)
-			goto bio_error;
+		for (i = 0; i < SSR_NUM_PHYSICAL_DISKS; i++) {
+			/* Write Data */
+			if (_ssr_submit_bio(bio, dev->physical_disks[i]) != 0)
+				goto bio_error;
+			/* Read relevant CRC sectors */
+			if (_ssr_submit_bio(bio_crc32, dev->physical_disks[i]) != 0)
+				goto bio_error;
+			/* Compute & Update CRC sectors */
+			ssr_compute_crc32(bio, bio_crc32);
+			/* Write back CRC sectors */
+			bio_crc32->bi_opf = REQ_OP_WRITE;
+			if (_ssr_submit_bio(bio_crc32, dev->physical_disks[i]) != 0)
+				goto bio_error;
+		}
+	}
+	
+	if (bio_crc32) {
+		bio_free_pages(bio_crc32);
+		bio_put(bio_crc32);
 	}
 
 	bio_endio(bio);
+	mutex_unlock(&ssr_dev.lock);
 	return BLK_QC_T_NONE;
 
 bio_error:
+	if (bio_crc32) {
+		bio_free_pages(bio_crc32);
+		bio_put(bio_crc32);
+	}
+
 	bio_io_error(bio);
+	mutex_unlock(&ssr_dev.lock);
 	return BLK_QC_T_NONE;
 }
 
@@ -163,6 +283,9 @@ static void delete_block_device(struct ssr_block_dev *dev)
 static int __init ssr_init(void)
 {
 	int err = 0;
+	int i;
+
+	mutex_init(&ssr_dev.lock);
 
 	err = register_blkdev(SSR_MAJOR, SSR_MODULE_NAME);
 	if (err < 0) {
@@ -184,9 +307,9 @@ static int __init ssr_init(void)
 	}
 
 	/* Get physical disks 'block_device' struct */
-	ssr_dev.physical_disk1 = blkdev_get_by_path(PHYSICAL_DISK1_NAME, FMODE_READ|FMODE_WRITE, THIS_MODULE);
-	ssr_dev.physical_disk2 = blkdev_get_by_path(PHYSICAL_DISK2_NAME, FMODE_READ|FMODE_WRITE, THIS_MODULE);
-	if (!ssr_dev.physical_disk1 || !ssr_dev.physical_disk2) {
+	ssr_dev.physical_disks[0] = blkdev_get_by_path(PHYSICAL_DISK1_NAME, FMODE_READ|FMODE_WRITE, THIS_MODULE);
+	ssr_dev.physical_disks[1] = blkdev_get_by_path(PHYSICAL_DISK2_NAME, FMODE_READ|FMODE_WRITE, THIS_MODULE);
+	if (!ssr_dev.physical_disks[0] || !ssr_dev.physical_disks[1]) {
 		pr_err("SSR: Failed to access physical disk. blkdev_get_by_path() failed.\n");
 		err = -EINVAL;
 		goto error;
@@ -201,10 +324,9 @@ static int __init ssr_init(void)
 	return 0;
 
 error:
-	if (ssr_dev.physical_disk1)
-		blkdev_put(ssr_dev.physical_disk1, FMODE_READ|FMODE_WRITE);
-	if (ssr_dev.physical_disk2)
-		blkdev_put(ssr_dev.physical_disk2, FMODE_READ|FMODE_WRITE);
+	for (i = 0; i < SSR_NUM_PHYSICAL_DISKS; i++)
+		if (ssr_dev.physical_disks[i])
+			blkdev_put(ssr_dev.physical_disks[i], FMODE_READ|FMODE_WRITE);
 
 	if (ssr_dev.physical_wq)
 		destroy_workqueue(ssr_dev.physical_wq);
@@ -216,10 +338,12 @@ error:
 
 static void ssr_exit(void)
 {
+	int i;
+
 	delete_block_device(&ssr_dev);
 
-	blkdev_put(ssr_dev.physical_disk1, FMODE_READ|FMODE_WRITE);
-	blkdev_put(ssr_dev.physical_disk2, FMODE_READ|FMODE_WRITE);
+	for (i = 0; i < SSR_NUM_PHYSICAL_DISKS; i++)
+		blkdev_put(ssr_dev.physical_disks[i], FMODE_READ|FMODE_WRITE);
 
 	destroy_workqueue(ssr_dev.physical_wq);
 	bioset_exit(&ssr_dev.ssr_bioset);
